18c1347ff40c3bcedc575c7cac8575a34c00760f
Adriel Andrade <adriel_hp.andrade@outlook.com>
2021-08-10 07:50:15 -0300

    adding base methods to CategoryForm

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { CategoryService } from './../shared/category.service';
+import { Category } from './../shared/category.model';
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
-  styleUrls: ['./category-form.component.scss']
+  styleUrls: ['./category-form.component.scss'],
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = [];
+  submittingForm: boolean = false;
+  category: Category = new Category();
 
-  constructor() { }
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
   }
 
+  ngAfterContentChecked(): void {
+    this.setPageTitle();
+  }
+
+  //PRIVATE METHODS
+  private setCurrentAction() {
+    if (this.route.snapshot.url[0].path === 'new') {
+      this.currentAction = 'new';
+    } else {
+      this.currentAction = 'edit';
+    }
+  }
+
+  private buildCategoryForm() {
+    this.categoryForm = this.fb.group({
+      if: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null],
+    });
+  }
+
+  private loadCategory() {
+    if (this.currentAction === 'edit') {
+      this.route.paramMap
+        .pipe(
+          switchMap((params) => this.categoryService.getById(+params.get('id')))
+        )
+        .subscribe(
+          (responseCategory) => {
+            this.category = responseCategory;
+            this.categoryForm.patchValue(this.category); // binds loaded category data to CategoryForm
+          },
+          (error) => console.log(error)
+        );
+    }
+  }
+
+  private setPageTitle() {
+    if (this.currentAction === 'new') {
+      this.pageTitle = 'Cadastro de Nova Categoria';
+    } else {
+      const categoryName = this.category.name || '';
+      this.pageTitle = 'Editando Categoria: ' + categoryName;
+    }
+  }
 }
