da29583f99d210042a294129215c38860dc221f9
Adriel Andrade <adriel_hp.andrade@outlook.com>
2021-09-03 16:42:33 -0300

    improving EntryListComponent with BaseResourceListComponent

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,39 +1,15 @@
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 import { Entry } from './../shared/entry.model';
 import { EntryService } from './../shared/entry.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-entry-list',
   templateUrl: './entry-list.component.html',
   styleUrls: ['./entry-list.component.scss'],
 })
-export class EntryListComponent implements OnInit {
-  entries: Entry[] = [];
-
-  constructor(private entryService: EntryService) {}
-
-  ngOnInit(): void {
-    this.entryService.getAll().subscribe(
-      (responseEntries) => {
-        this.entries = responseEntries.sort((a, b) => b.id - a.id);
-      },
-      (error) => console.log(`ERRO AO CARREGAR A LISTA => ${error}`)
-    );
-  }
-
-  deleteEntry(entry: any) {
-    const mustDelete = confirm('Deseja realmente excluir este item?');
-
-    if (mustDelete) {
-      this.entryService.delete(entry.id).subscribe(
-        () => {
-          this.entries = this.entries.filter((element) => element !== entry);
-        },
-        (error) => {
-          alert('ERRO AO EXCLUIR CATEGORY');
-          console.log(error);
-        }
-      );
-    }
+export class EntryListComponent extends BaseResourceListComponent<Entry> {
+  constructor(private entryService: EntryService) {
+    super(entryService);
   }
 }
